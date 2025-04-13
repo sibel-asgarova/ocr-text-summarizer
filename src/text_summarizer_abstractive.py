e31665856fbd6b4c88d4c216caa0ff6128aee855
Sibel Asgarova <sibelasgarova@gmail.com>
2025-04-13 23:33:16 +0400

    minor changes

--- src/text_summarizer_abstractive.py
+++ src/text_summarizer_abstractive.py
@@ -12,7 +12,7 @@ def summarize_text_t5(text):
     inputs = tokenizer(input_text, return_tensors="pt", max_length=1024, truncation=True)
     
  
-    summary_ids = model.generate(inputs["input_ids"], max_length=300, num_beams=5, early_stopping=True)
+    summary_ids = model.generate(inputs["input_ids"], max_length=500, num_beams=5, early_stopping=True)
     
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
